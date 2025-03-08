cb806d7302a6e729c7d521af11333650ab93befa
Shivangi Kachole <shivangikacholevu@gmail.com>
2025-03-08 19:15:07 +0100

    initial implementation of video and linguistic

--- src/agent/emotion/linguistic.py
+++ src/agent/emotion/linguistic.py
@@ -1,7 +1,24 @@
+from transformers import pipeline
+from src.agent.asr import asr
+
 class LinguisticSystem:
     def __init__(self):
-        pass
+        self.asr = asr.ASR()
+        self.emotion = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base")
 
     def get_emotion(self, audio):
-        pass
+        transcription = self.asr.transcribe(audio)
+        try:
+            emotion = self.emotion(transcription)
+            if not emotion:
+                return "Unknown"
+            else:
+                return emotion[0].get('label', 'Unknown')
+        except Exception as e:
+            print(f"Error in emotion detection: {e}")
+            return "Unknown"
+
+
+
+
 
--- src/agent/emotion/video.py
+++ src/agent/emotion/video.py
@@ -1,6 +1,47 @@
+from deepface import DeepFace
+import cv2
+import time
+
 class VideoSystem:
     def __init__(self):
-        pass
+        self.capture = None
 
-    def get_emotion(self, video):
-        pass
+    def get_emotion(self, video=None, utterance_length=5):
+        if video is None:
+            capture = cv2.VideoCapture(0)
+        else:
+            capture = cv2.VideoCapture(video)
+
+        start = time.time()
+        detectedEmotions = []
+
+        while capture.isOpened():
+            ret, frame = capture.read()
+            if not ret:
+                break
+
+            elapsedTime = time.time() - start
+            if elapsedTime >= utterance_length:
+                break
+
+            try:
+                analysis = DeepFace.analyze(frame, actions=['emotion'], enforce_detection=False)
+                dominantEmotion = analysis[0].get('dominant_emotion', 'Unknown')
+                detectedEmotions.append(dominantEmotion)
+            except Exception as e:
+                print(f"Error: {e}. No face detected.")
+
+        capture.release()
+        cv2.destroyAllWindows()
+
+
+        if detectedEmotions:
+            return max(detectedEmotions, key=detectedEmotions.count)
+        else:
+            return None
+
+
+
+
+
+
