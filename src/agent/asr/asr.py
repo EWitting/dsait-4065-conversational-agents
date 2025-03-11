fb0ace2c794ed491ea862e443f9803f750f0fefd
denizcetinnn <86082206+denizcetinnn@users.noreply.github.com>
2025-03-11 19:02:44 +0100

    comments on asr.py

--- src/agent/asr/asr.py
+++ src/agent/asr/asr.py
@@ -3,10 +3,10 @@ import warnings
 warnings.filterwarnings("ignore", message="FP16 is not supported on CPU; using FP32 instead")
 
 class ASR:
-    def __init__(self, model_name: str = "base"):
+    def __init__(self, model_name: str = "base"):#initialize whisper base model
         self.model = whisper.load_model(model_name)  
 
-    def transcribe(self, prompt: str, audio: str) -> str:
+    def transcribe(self, prompt: str, audio: str) -> str:#transciption function, taking the audio file and the initial prompt as input
         result = self.model.transcribe(audio, initial_prompt= prompt)
         return result.get("text", "")
 
