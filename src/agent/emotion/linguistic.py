90289fe06b216fee3b96599f1a6b5dfd041b7cf5
denizcetinnn <86082206+denizcetinnn@users.noreply.github.com>
2025-03-11 19:09:31 +0100

    comments on linguistic.py

--- src/agent/emotion/linguistic.py
+++ src/agent/emotion/linguistic.py
@@ -3,13 +3,13 @@ from transformers import pipeline
 
 class LinguisticSystem:
     def __init__(self):
-        self.emotion = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base")
+        self.emotion = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base") #feed the text to the classifier to infer emotion
 
 
     def get_emotion(self, text):
-        emotion = self.emotion(text)
+        emotion = self.emotion(text)#usage of the function
         if not emotion:
             return "Unknown"
         else:
-            return emotion[0].get('label', 'Unknown')
+            return emotion[0].get('label', 'Unknown')#return the emotion
 
