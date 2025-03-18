aa04b12738b200313293a1437ae693491b87b147
thekaasking <razovanberkel@gmail.com>
2025-03-18 19:56:11 +0100

    linter + fix window close error

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import tkinter as tk
+from tkinter import messagebox
 from dotenv import load_dotenv
-
+import sys
 from src.gui.fashionAssistantGUI import FashionAssistantGUI
+
 load_dotenv()
 
 if __name__ == "__main__":
     root = tk.Tk()
+
+    def on_closing():
+        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+            root.destroy()
+            sys.exit(0)
+
+    root.protocol("WM_DELETE_WINDOW", on_closing)
     app = FashionAssistantGUI(root)
     root.mainloop()
